975b647969cf040410716ff5e1add70cfd47d9ee
Marco <msmo1960@gmail.com>
2021-08-03 14:54:12 -0500

    testing was completed, but failed

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test
+ // First Testw
+ it('renders', () => {
+    render(<About />);
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+      }
+
+
+  });
+ 
+  // Second Test
+
+  })
+
+
